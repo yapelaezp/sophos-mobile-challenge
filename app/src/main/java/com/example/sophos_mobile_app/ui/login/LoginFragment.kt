f580beb5375dde37de4789822e8edcceaeaa1ae7
Yesid Alejandro Pelaez <yapelaezp@unal.edu.co>
2022-12-25 17:26:10 -0500

    Logout with datastore implemented

--- app/src/main/java/com/example/sophos_mobile_app/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/sophos_mobile_app/ui/login/LoginFragment.kt
@@ -1,13 +1,10 @@
 package com.example.sophos_mobile_app.ui.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
+import android.view.*
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -28,9 +25,10 @@ class LoginFragment : Fragment() {
 
     private val loginViewModel: LoginViewModel by viewModels()
 
-    companion object{
+    companion object {
         const val EMAIL = "email"
         const val PASSWORD = "password"
+        const val NAME = "name"
         const val BIOMETRIC_EMAIL = "biometric_email"
         const val BIOMETRIC_PASSWORD = "biometric_password"
     }
@@ -45,15 +43,35 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        isUserLogged()
         setListeners()
         observeViewModel()
         return binding.root
     }
 
+    private fun isUserLogged() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            getDataStorePreferences()?.collect() { userData ->
+                println(userData)
+                if (userData.first.isNotEmpty()) {
+                    withContext(Dispatchers.Main){
+                        val action = LoginFragmentDirections.actionToMenuFragmentDestination(userData.third, userData.first)
+                        findNavController().navigate(action)
+                    }
+                }
+            }
+        }
+    }
+
     private fun observeViewModel() {
         val userEmail = binding.etvLoginEmail.text.toString()
-        loginViewModel.user.observe(viewLifecycleOwner){ user ->
-            val action = LoginFragmentDirections.actionToMenuFragmentDestination(user.name, userEmail)
+        val userPassword = binding.etvLoginPassword.text.toString()
+        loginViewModel.user.observe(viewLifecycleOwner) { user ->
+            lifecycleScope.launch(Dispatchers.IO) {
+                saveUserInDataStore(userEmail, userPassword, user.name)
+            }
+            val action =
+                LoginFragmentDirections.actionToMenuFragmentDestination(user.name, userEmail)
             findNavController().navigate(action)
         }
     }
@@ -67,60 +85,45 @@ class LoginFragment : Fragment() {
             if (validateEmail(email) && validatePassword(password)) {
                 loginViewModel.login(email, password)
             }
-            shouldSaveUserInDataStore(email, password)
-        }
-    }
-
-    private fun shouldSaveUserInDataStore(email: String, password: String) {
-        lifecycleScope.launch(Dispatchers.IO){
-            getDataStorePreferences()?.collect(){ preference ->
-                println(preference)
-                if (preference != email){
-                    saveInDataStore(email, password)
-                    withContext(Dispatchers.Main){
-                        Toast.makeText(requireContext(), "Data saved in datastore", Toast.LENGTH_SHORT).show()
-                    }
-                }else {
-                    withContext(Dispatchers.Main){
-                        Toast.makeText(requireContext(), "Data already saved", Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
         }
     }
 
     private fun validatePassword(password: String): Boolean {
-        return if (Validation.isFieldEmpty(password)){
+        return if (Validation.isFieldEmpty(password)) {
             binding.tilLoginPassword.error = "Password can\'t be empty"
             false
-        } else{
+        } else {
             binding.tilLoginPassword.error = null
             true
         }
     }
 
     private fun validateEmail(email: String): Boolean {
-        return if (Validation.isFieldEmpty(email)){
+        return if (Validation.isFieldEmpty(email)) {
             binding.tilLoginEmail.error = "Email can\'t be empty"
             false
-        } else if (!Validation.isEmailValid(email)){
+        } else if (!Validation.isEmailValid(email)) {
             binding.tilLoginEmail.error = "Invalid email"
             false
-        } else{
+        } else {
             binding.tilLoginEmail.error = null
             true
         }
     }
 
     private fun getDataStorePreferences() = context?.dataStore?.data?.map { preferences ->
-        preferences[stringPreferencesKey(EMAIL)].orEmpty()
+        Triple(
+            preferences[stringPreferencesKey(EMAIL)].orEmpty(),
+            preferences[stringPreferencesKey(PASSWORD)].orEmpty(),
+            preferences[stringPreferencesKey(NAME)].orEmpty()
+        )
     }
 
-
-    private suspend fun saveInDataStore(email: String, password: String){
+    private suspend fun saveUserInDataStore(email: String, password: String, name: String) {
         context?.dataStore?.edit { preferences ->
             preferences[stringPreferencesKey(EMAIL)] = email
             preferences[stringPreferencesKey(PASSWORD)] = password
+            preferences[stringPreferencesKey(NAME)] = name
             preferences[stringPreferencesKey(BIOMETRIC_EMAIL)] = ""
             preferences[stringPreferencesKey(BIOMETRIC_PASSWORD)] = ""
         }
--- app/src/main/java/com/example/sophos_mobile_app/ui/menu/MenuFragment.kt
+++ app/src/main/java/com/example/sophos_mobile_app/ui/menu/MenuFragment.kt
@@ -1,16 +1,21 @@
 package com.example.sophos_mobile_app.ui.menu
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.datastore.preferences.core.edit
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.sophos_mobile_app.R
 import com.example.sophos_mobile_app.databinding.FragmentMenuBinding
+import com.example.sophos_mobile_app.utils.dataStore
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class MenuFragment : Fragment() {
@@ -31,10 +36,12 @@ class MenuFragment : Fragment() {
     }
 
     private fun setListeners() {
+
         binding.btnMenuScreenSendDocs.setOnClickListener {
             val action = MenuFragmentDirections.actionToSendDocumentsFragmentDestination(null)
             findNavController().navigate(action)
         }
+
         binding.btnMenuScreenSeeDocs.setOnClickListener {
             val action =
                 MenuFragmentDirections.actionMenuFragmentDestinationToViewDocumentsFragmentDestination(
@@ -43,6 +50,34 @@ class MenuFragment : Fragment() {
                 )
             findNavController().navigate(action)
         }
+
+        binding.toolbarMenuScreen.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.action_logout -> {
+                    lifecycleScope.launch(Dispatchers.IO){
+                        logout()
+                    }
+                    true
+                }
+                R.id.action_language -> {
+                    println("Action language")
+                    true
+                }
+                R.id.action_mode -> {
+                    println("Action mode")
+                    true
+                }
+                R.id.action_see_docs -> {
+                    println("See docs")
+                    true
+                }
+                R.id.action_offices -> {
+                    println("Action offices")
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun setComponents() {
@@ -50,6 +85,17 @@ class MenuFragment : Fragment() {
         binding.toolbarMenuScreen.title = args.userName
         binding.toolbarMenuScreen.overflowIcon =
             ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_menu_24)
+    }
+
+    private suspend fun logout(){
+        context?.dataStore?.edit { preferences ->
+            preferences.clear()
+        }
+        withContext(Dispatchers.Main){
+            Toast.makeText(requireContext(), "Data cleared successfully", Toast.LENGTH_SHORT).show()
+            val action = MenuFragmentDirections.actionMenuFragmentDestinationToLoginFragmentDestination()
+            findNavController().navigate(action)
+        }
     }
 
 }
