6dde90361249c37e505e8217934d06e8264e0f6a
Yesid Alejandro Pelaez <yapelaezp@unal.edu.co>
2023-01-01 03:20:20 -0500

    Language feature implemented

---
+++ app/src/main/java/com/example/sophos_mobile_app/utils/AppLanguage.kt
@@ -0,0 +1,30 @@
+package com.example.sophos_mobile_app.utils
+
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.os.LocaleListCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.util.*
+import javax.inject.Inject
+
+
+class AppLanguage @Inject constructor() {
+    //val appLocale: LocaleListCompat = LocaleListCompat.forLanguageTags("xx-YY")
+    // Fetching the current application locale using the AndroidX support Library
+    private var _currentLocaleName = if (!AppCompatDelegate.getApplicationLocales().isEmpty) {
+        // Fetches the current Application Locale from the list
+        AppCompatDelegate.getApplicationLocales()[0]?.displayName
+    } else {
+        // Fetches the default System Locale
+        Locale.getDefault().displayName
+    }
+     val currentLocaleName get() = _currentLocaleName
+
+    suspend fun changeLanguage(language: String){
+        withContext(Dispatchers.Main){
+            val localeList = LocaleListCompat.forLanguageTags(language)
+            AppCompatDelegate.setApplicationLocales(localeList)
+        }
+    }
+
+}
